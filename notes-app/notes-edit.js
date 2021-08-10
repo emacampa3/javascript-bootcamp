f23de57b87b94be57065030ef9a4520c321f6cec
Ema Čampa <emacampa3@gmail.com>
2021-08-10 12:43:35 +0200

    some changes

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,3 +1,6 @@
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1) // getting the entire id without '#' (starting at the second place)
 const notes = getSavedNotes() // getting all of the data in local storage and rendering
 const note = notes.find(function (note) { 
@@ -8,5 +11,21 @@ if (note === undefined) {
     location.assign('index.html') // if there is no match, redirect to home page
 }
 
-document.querySelector('#note-title').value = note.title
-document.querySelector('#note-body').value = note.body
+titleElement.value = note.title
+bodyElement.value = note.body
+
+titleElement.addEventListener('input', function (e) {
+    note.title = e.target.value
+    saveNotes(notes)
+})
+
+bodyElement.addEventListener('input', function (e) {
+    note.body = e.target.value
+    saveNotes(notes)
+})
+
+removeElement.addEventListener('click', function (e) {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('index.html')
+})
