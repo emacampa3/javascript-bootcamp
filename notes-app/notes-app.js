ece399122cc5c7ba146f4ffdef094d3af4093abb
Ema Čampa <emacampa3@gmail.com>
2021-08-10 18:25:14 +0200

    new changes

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,4 +1,4 @@
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
 const filters = {
     searchText: ''
@@ -25,4 +25,11 @@ document.querySelector('#search-text').addEventListener('input', function(e) { /
 
 document.querySelector('#filter-by').addEventListener('change', function(e) {
     console.log(e.target.value) // information lives in e.target.value
+})
+
+window.addEventListener('storage', function (e) {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue) // new storage value for the key
+        renderNotes(notes, filters)
+    }
 })
--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -3,14 +3,13 @@ const bodyElement = document.querySelector('#note-body')
 const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1) // getting the entire id without '#' (starting at the second place)
 const notes = getSavedNotes() // getting all of the data in local storage and rendering
-const note = notes.find(function (note) { 
+let note = notes.find(function (note) { 
     return note.id === noteId // if note id matches a note, return true
 }) 
 
 if (note === undefined) {
     location.assign('index.html') // if there is no match, redirect to home page
 }
-
 titleElement.value = note.title
 bodyElement.value = note.body
 
@@ -28,4 +27,19 @@ removeElement.addEventListener('click', function (e) {
     removeNote(note.id)
     saveNotes(notes)
     location.assign('index.html')
+})
+
+window.addEventListener('storage', function (e) { // global event listener for when any data in localStorage changes
+    if (e.key === 'notes')  {
+        notes = JSON.parse(e.newValue) // parsing the data stored in a new value
+        let note = notes.find(function (note) { 
+            return note.id === noteId // if note id matches a note, return true
+        }) 
+        
+        if (note === undefined) {
+            location.assign('index.html') // if there is no match, redirect to home page
+        }
+        titleElement.value = note.title
+        bodyElement.value = note.body
+    }
 })
