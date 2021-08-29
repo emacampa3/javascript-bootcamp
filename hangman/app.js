6b86b662438a3f1c8e7f345b08ffc51a12abc72d
Ema Čampa <emacampa3@gmail.com>
2021-08-29 16:46:01 +0200

    HTTP requests from JavaScript

--- hangman/app.js
+++ hangman/app.js
@@ -11,3 +11,20 @@ window.addEventListener('keypress', function (e) {
     puzzleEl.textContent = game1.puzzle// displays the puzzle
     guessesEl.textContent = game1.statusMessage
 })
+
+// HTTP (Hypertext Transfer Protocol)
+// request: what do we want to do
+// response: what was actually done
+
+// making an HTTP request from JavaScript code
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', () => {
+    if (e.target.readyState === 4) { // if status of readyState equals 4, execute the following
+        const data = JSON.parse(e.target.responseText)
+        console.log(data) // prints the object: whatever text the io inputs as a word nedded to be guessed
+    }
+})
+
+request.open('GET', 'http:puzzle.mead.io/puzzle')
+request.send()
