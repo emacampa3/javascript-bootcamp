a9a7a691ef56052fcce59b3ac73b67981c6ed30e
Ema Čampa <emacampa3@gmail.com>
2021-08-30 20:23:05 +0200

    promise chaining explained

--- functions/promises.js
+++ functions/promises.js
@@ -1,34 +1,52 @@
 // Callback
-const getDataCallback = (callback) => {
+const getDataCallback = (num, callback) => {
     setTimeout(() => {
-        // callback(undefined, 'This is the data')
-        callback('This is my callback error', undefined)
+        if (typeof num === 'number') {
+            callback(undeifned, num * 2)
+        } else {
+            callback('Number must be provided')
+        }
     }, 2000) // wait 2000ms
 }
-// calling the function
-getDataCallback((error, data) => {
+// calling the function: callback hell: unnecessarily complex
+getDataCallback(2, (error, data) => {
     if (error) {
-
+        console.log(error)
     } else {
-        console.log(data)
+        getDataCallback(data, (error, data) => {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log(data)
+            }
+        })
     }
 })
 
 
 // Promise API
-const getDataPromise = () => new Promise((resolve, reject) => { // resolve: called when things go well; reject: called when something went poorly
+// with promises we can reject/resolve the promise a single time
+// if we have two calls for then, we get two outputs, but at the same time
+const getDataPromise = (num) => new Promise((resolve, reject) => { // resolve: called when things go well; reject: called when something went poorly
     setTimeout(() => {
-        // resolve('This is the promise data.')
-        reject('This is my promise error') // we either pass in resolve or reject, never both
+        typeof num === 'number' ? resolve(num * 2) : reject('Number must be provided')
     }, 2000)
 })
 
-const myPromise = getDataPromise()
-// calling the function
-myPromise.then((data) => { // function only fires when the promise resolves
-    console.log(data)
+getDataPromise(2).then((data) => {
+    getDataPromise(data).then((data) => {
+        console.log(`Promise data: ${data}`)
+    }, (error) => {
+        console.log(error)
+    })
 }, (error) => {
-    console.log(error) // function runs when the promise rejects
+    console.log(error)
 })
-// with promises we can reject/resolve the promise a single time
-// if we have two calls for then, we get two outputs, but at the same time
+
+getDataPromise(10).then((data) => {
+    return getDataPromise(data)
+}).then((data) => { // promise chaining
+    console.log(data)
+}).catch((error) => { // a single error handler for all promises: if the first one failes, it will return an error, not run all other functions
+console.log(error)
+})
